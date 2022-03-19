3a1228a5b48a46ba301b0f450b242281b8be1bae
Ramona Snider <rls2061@yahoo.com>
2022-03-19 08:03:25 -0700

    added index.js to fies and changed header alignment and border radius on pictures

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+let header = document.querySelector('#page-header')//"find the page header"
+header.style.textAlign = "left"  //Move header to left alignment
 
-// Select some elements...
+let dogImages = document.querySelectorAll('.dog-image')//"find all the dog images your document"
+for(let i =0; i < dogImages.length; i++){
+    dogImages[i].style.borderRadius = '50px'//"add a border radius around the pictures"
+}
